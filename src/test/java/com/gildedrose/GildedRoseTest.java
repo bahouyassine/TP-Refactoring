e51191ecc1b6d4d735c489c8774742c031109235
yabahou <yabahou@ada.local.isima.fr>
2023-09-15 14:43:05 +0200

    add 2 tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,61 @@ class GildedRoseTest {
     Item element = new Item("foo", 0, 0);
     GildedRose app = new GildedRose(new Item[] {element});
     app.updateQuality();
-    assertEquals("FIXME", element.name, "the name changed");
+    assertEquals("foo", element.name, "the name changed");
+  }
+  
+
+  @Test
+  @DisplayName("Test that quality is positive after update")
+  void test_quality_positive() {
+    Item element = new Item("foo", 0, 0);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertTrue(element.quality>=0,"sellIn is negative" );
+
   }
 
+  @Test
+  @DisplayName("Test that quality is less then 50 ")
+  void test_quality_less_then_50() {
+    Item element = new Item("Aged Brie", 0, 49);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertTrue(element.quality <=50,"La qualité supérieur à 50" );
+
+  }
+
+
+  
+  @Test
+  @DisplayName("Test that quality of Aged Brie increases proproly")
+  void test_quality_invrease_Aged_Brie() {
+    Item[] items = new Item[] {
+      new Item("Aged Brie", 15, 20), //
+      new Item("Aged Brie", 9, 20), //
+      new Item("Aged Brie", 4, 20), //
+      new Item("Aged Brie", 0, 20), //
+     };
+
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals(21,items[0].quality,"Value needs to be increased to 21" );
+    assertEquals(22,items[1].quality,"Value needs to be increased to 22" );
+    assertEquals(23,items[2].quality,"Value needs to be increased to 23" );
+    // assertEquals(0,items[2].quality,"Value needs to be increased by 0" );
+
+  
+  }
+
+
+
+  @Test
+  @DisplayName("Test that quality of Aged Brie increases proproly")
+  void test_quality_invrease_Backstage_passes() {
+    Item element = new Item("Aged Brie", 12, 0);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertEquals(1,element.quality,"" );
+  }
+  
 }
