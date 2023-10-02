7b604af288bd44b1de8d24dd5a785dbadc147ef8
ayoubhatmi <ayoub.hatmi18@gmail.com>
2023-10-02 07:24:54 +0200

    Define constants

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -3,6 +3,9 @@ package com.gildedrose;
 class GildedRose {
   public static final int MINIMUM_QUALITY = 0;
   public static final int MAXIMUM_QUALITY = 50;
+  public static final String AGED_BRIE = "Aged Brie";
+  public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+  public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
   Item[] items;
 
   public GildedRose(Item[] items) {
@@ -11,10 +14,10 @@ class GildedRose {
 
   public void updateQuality() {
     for (int i = 0; i < items.length; i++) {
-      if (!items[i].name.equals("Aged Brie")
-          && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      if (!items[i].name.equals(AGED_BRIE)
+          && !items[i].name.equals(BACKSTAGE)) {
         if (items[i].quality > MINIMUM_QUALITY) {
-          if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+          if (!items[i].name.equals(SULFURAS)) {
             items[i].quality = items[i].quality - 1;
           }
         }
@@ -22,7 +25,7 @@ class GildedRose {
         if (items[i].quality < MAXIMUM_QUALITY) {
           items[i].quality = items[i].quality + 1;
 
-          if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+          if (items[i].name.equals(BACKSTAGE)) {
             if (items[i].sellIn < 11) {
               if (items[i].quality < MAXIMUM_QUALITY) {
                 items[i].quality = items[i].quality + 1;
@@ -38,15 +41,15 @@ class GildedRose {
         }
       }
 
-      if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+      if (!items[i].name.equals(SULFURAS)) {
         items[i].sellIn = items[i].sellIn - 1;
       }
 
       if (items[i].sellIn < 0) {
-        if (!items[i].name.equals("Aged Brie")) {
-          if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (!items[i].name.equals(AGED_BRIE)) {
+          if (!items[i].name.equals(BACKSTAGE)) {
             if (items[i].quality > MINIMUM_QUALITY) {
-              if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+              if (!items[i].name.equals(SULFURAS)) {
                 items[i].quality = items[i].quality - 1;
               }
             }
